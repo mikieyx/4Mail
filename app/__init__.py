8fedf5a5880487eec7b911d98f822384e58ac14e
mikieyx <mikieyx@gmail.com>
2023-04-21 12:25:09 -0700

    Database setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
+from app import routes, models
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 myapp_obj = Flask(__name__)
-# myapp_obj.config.from_mapping(..)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+myapp_obj.config.from_mapping(
+    SECRET_KEY='4mail',
+    SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'app.db'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+)
 
 db = SQLAlchemy(myapp_obj)
-
-from app import routes, models
