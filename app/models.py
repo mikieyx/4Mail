a5f4b94bffaca575e971f51bd8ef115d181240cb
Anh Nguyen <107016480+AlexNguyenSJSU@users.noreply.github.com>
2023-05-05 11:43:37 -0700

    Update models.py with my code from my remote branch 'password_reset'

--- app/models.py
+++ app/models.py
@@ -12,7 +12,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(32), nullable=False)
     password = db.Column(db.String(32), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    task = db.relationship('Task', backref='author', lazy='dynamic')
+
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -28,14 +29,23 @@ class User(db.Model, UserMixin):
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, myapp_obj.config['SECRET_KEY'], algorithms=[
-                            'HS256'])['reset_password']
+            id = jwt.decode(token, myapp_obj.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
 
     def __repr__(self):
         return f'<User {self.id}: {self.username}>'
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Post {self.id}: {self.body}>'
 
 
 class Email(db.Model):
