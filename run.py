6d88c00ecb2ce7a581b89d5e5179b7c7c1c4b132
Van Duong <aduong421@gmail.com>
2023-05-10 01:38:55 -0700

    Complete chat room

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-from app import myapp_obj
+from app import myapp_obj, socketio
 
 # Run our Flask application:
-myapp_obj.run(debug=True)
+#myapp_obj.run(debug=True)
+
+
+if __name__ == "__main__":
+    socketio.run(myapp_obj, debug=True)
